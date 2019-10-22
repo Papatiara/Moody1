79c1d07b9be720f0c1e01f8ac52569f057cb32e9
Patricia Shirazi <pshirazi@google.com>
2019-10-22 16:33:31 -0700

    Eliminated alert functions, made first page responsive

--- src/Input.js
+++ src/Input.js
@@ -9,25 +9,31 @@ class Input  extends React.Component {
 
     this.state = {
       value: "",
-      hasValue: false
+      hasValue: false,
+      required: ""
     };
       this.handleChange = this.handleChange.bind(this); 
       this.handleSubmit = this.handleSubmit.bind(this);
+      this.handleClick = this.handleClick.bind(this);
   }
-	handleChange(event) {
-	  this.setState({value: event.target.value});
+  handleChange(event) {
+    this.setState({value: event.target.value});
   }
 
   handleSubmit(event){
-    if (this.state.value.length === 0) {
-      alert('The input requires a value');
-     } if (this.state.value.length < 20) {
-       alert('The input should have at least 20 characters')
+    if (this.state.value.length < 20) {
+      this.setState({required: "It's required to enter a text with at least 20 characters"});
      } else {
       this.setState({hasValue: true});
      }
    }
-   
+    
+  handleClick(event) {
+    console.log(this.state.required)
+    if (this.state.required.length > 0) {
+      this.setState({required: " "});
+    }
+  }
   
 render() {
   if (this.state.hasValue === true) {
@@ -48,7 +54,8 @@ render() {
     <div className="wrapper">
       <form >
         <label>
-          <TextareaAutosize className="text-area" value={this.state.value} onChange={this.handleChange} />
+          <TextareaAutosize className="text-area" value={this.state.value} onChange={this.handleChange} onClick={this.handleClick}/>
+	  <p className="required-field"> {this.state.required} </p>
           <input className="input" value="Submit" onClick={this.handleSubmit}/>
         </label>
       </form>
