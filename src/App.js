d3e91a5f42e49ea629d6b165ec648dd21e081610
Patricia Shirazi <pshirazi@google.com>
2019-10-22 13:24:20 -0700

    added reload page on title - fixed positions and added some style

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import background from './moody.png';
 import './App.css';
 import Input from './Input';
 
+
 class App extends Component {
   render() {
     return (
       <div>
         <div className="App-header">
-            <h3 className="App-title"> Moody</h3>
+            <h3 className="App-title" onClick={() => window.location.reload(false)}> Moody</h3>
             <h3 className="App-contact">Contact</h3>
       </div>
       <Input />
